15e1086f7e7dd743764b15f1973a243fb518110c
jhpark1109 <jhpark970616@gmail.com>
2017-08-21 12:09:39 +0900

    Add files via upload

---
+++ 10815.cpp
@@ -0,0 +1,44 @@
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int N, M;
+vector<int> vec;
+bool binary(int n)
+{
+	int high=N-1;
+	int low=0;
+	int mid;
+	while(low<=high)
+	{
+		mid=(high+low)/2;
+		if(vec[mid]==n)
+			return true;
+		else if(vec[mid]>n)
+			high=mid-1;
+		else
+			low=mid+1;
+	}
+	return false;
+}
+int main()
+{
+	scanf("%d", &N);
+	vec.resize(N);
+	for(int i=0;i<N;i++)
+		scanf("%d", &vec[i]);
+	sort(vec.begin(), vec.end());
+
+	scanf("%d", &M);
+	for(int i=0;i<M;i++)
+	{
+		int n;
+		scanf("%d", &n);
+		if(binary(n))
+			printf("1 ");
+		else
+			printf("0 ");
+	}
+	return 0;
+}
+
